21726a58c8353ad70db8a99fcad99c92641a5bb2
김윤서(Yunseo Kim) <yunseo47@protonmail.ch>
2022-08-14 01:52:40 +0900

    Update unet.py
    - Increase U-Net model size
    - score: 0.9366

--- utils/model/unet.py
+++ utils/model/unet.py
@@ -10,10 +10,12 @@ class Unet(nn.Module):
         self.in_chans = in_chans
         self.out_chans = out_chans
 
-        self.first_block = ConvBlock(in_chans, 2)
-        self.down1 = Down(2, 4)
-        self.up1 = Up(4, 2)
-        self.last_block = nn.Conv2d(2, out_chans, kernel_size=1)
+        self.first_block = ConvBlock(in_chans, 5)
+        self.down1 = Down(5, 40)
+        self.down2 = Down(40, 320)
+        self.up1 = Up(320, 40)
+        self.up2 = Up(40, 5)
+        self.last_block = nn.Conv2d(5, out_chans, kernel_size=1)
 
     def norm(self, x):
         b, h, w = x.shape
@@ -30,9 +32,11 @@ class Unet(nn.Module):
         input, mean, std = self.norm(input)
         input = input.unsqueeze(1)
         d1 = self.first_block(input)
-        m0 = self.down1(d1)
-        u1 = self.up1(m0, d1)
-        output = self.last_block(u1)
+        d2 = self.down1(d1)
+        m0 = self.down2(d2)
+        u1 = self.up1(m0, d2)
+        u2 = self.up2(u1, d1)
+        output = self.last_block(u2)
         output = output.squeeze(1)
         output = self.unnorm(output, mean, std)
 
@@ -79,7 +83,7 @@ class Up(nn.Module):
         super().__init__()
         self.in_chans = in_chans
         self.out_chans = out_chans
-        self.up = nn.ConvTranspose2d(in_chans, in_chans // 2, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(in_chans, in_chans // 8 * 7, kernel_size=2, stride=2)
         self.conv = ConvBlock(in_chans, out_chans)
 
     def forward(self, x, concat_input):
